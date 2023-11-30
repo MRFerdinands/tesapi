a1ad8f31be3f29f0aa584cc896f4486ac0337b6e
MRFerdinands <rioqwerty120@gmail.com>
2023-11-30 11:31:35 +0700

    setup routes ke server.js

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const bodyParser = require("body-parser");
 const express = require("express");
+const { route } = require("express/lib/application");
 const app = express();
 
 // Parse app
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+var routes = require("./routes");
+routes.app;
 
 app.listen(3000, () => {
   console.log(`Server started on port`);
